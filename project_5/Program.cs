e3d9aab633c29e6bc5827544323fbf152cdf7763
z.oganesyan <z.oganesyan@talenttech.ru>
2023-03-12 17:50:39 +0500

    project_5

---
+++ project_5/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите  имя пользователя");
+string username = Console.ReadLine();
+
+if( username == "Маша")
+{
+  Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+  Console.Write("Привет, ");
+  Console.WriteLine(username);
+
+}
