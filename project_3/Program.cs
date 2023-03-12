803452d6bde2053db861e2fd2d3844edcbcdffd3
z.oganesyan <z.oganesyan@talenttech.ru>
2023-03-12 17:25:26 +0500

    Внесены изменения в project 3

--- project_3/Program.cs
+++ project_3/Program.cs
@@ -1,5 +1,8 @@
-﻿int numberA = 3;
-int numberB = 5;
-Console.WriteLine(numberA + numberB);
+﻿int numberA = new Random().Next(1, 10);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
 
 
