2de263fc902e39f1262b75b172c5271570198535
z.oganesyan <z.oganesyan@talenttech.ru>
2023-03-15 20:04:45 +0500

    добавлен пример с функциями

---
+++ project_6/Program.cs
@@ -0,0 +1,26 @@
+﻿int Max( int arg1, int arg2, int arg3 )
+{
+  int result arg1;
+  if ( arg2 < result ) result = arg2;
+  if ( arg3 < result ) result = arg3;
+  return result
+}
+
+int a1 = 44;
+int b1 = 33;
+int c1 = 43;
+int a2 = 14;
+int b2 = 34;
+int c2 = 99;
+int a3 = 84;
+int b3 = 38;
+int c3 = 19;
+
+int max1 = Max ( a1, b1, c1 );
+int max2 = Max ( a2, b2, c2 );
+int max3 = Max ( a3, b3, c3 );
+int max = Max ( max1, max2, max3)
+
+Console.WriteLine(max)
+
+
